13eb428adf3e5af43d808496d38c66221b5b1337
MD.HA-MIM BHUIYAN <mdabuhamimbhuiyan@gmail.com>
2025-08-24 13:05:54 +0600

    Add files via upload

---
+++ app/shop/page.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Banner from '../components/Shop/Banner'
+import Products from '../components/Shop/Products'
+
+const page = () => {
+  return (
+   <>
+     <Banner/>
+     <Products/>
+   </>
+  )
+}
+
+export default page
